1e37708e04cc0e318a284080d5368498560e0d3d
juliocsar13 <jc_redcsar@hotmail.com>
2015-03-15 23:54:53 -0500

    compiler slidingPanelLayout D: funciona D:

--- app/src/main/java/com/example/lenovo/deslid_button/MainActivity.java
+++ app/src/main/java/com/example/lenovo/deslid_button/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout);
+        setContentView(R.layout.layout3);
 
   /*      username = (EditText) findViewById(R.id.usuario);
         Typeface fontusuario = Typeface.createFromAsset(getAssets(), "Balkeno.ttf");
